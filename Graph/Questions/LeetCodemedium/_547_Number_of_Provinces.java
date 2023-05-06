e9d2231b49ce255849ac0443da795470bd12e4a2
akshatarora0013 <akshatarora0013@gmail.com>
2023-05-06 23:58:46 +0530

    added one more solution for question 547

--- Graph/Questions/LeetCodemedium/_547_Number_of_Provinces.java
+++ Graph/Questions/LeetCodemedium/_547_Number_of_Provinces.java
@@ -1,5 +1,7 @@
 package Graph.Questions.LeetCodemedium;
 
+import java.util.*;
+
 //  https://leetcode.com/problems/number-of-provinces/description/
 
 public class _547_Number_of_Provinces {
@@ -24,4 +26,64 @@ public class _547_Number_of_Provinces {
             }
         }
     }
+
+    // using Disjoint Set
+//    class Solution {
+//        public int findCircleNum(int[][] Connected) {
+//            DisjointSet ds = new DisjointSet(Connected.length);
+//            for(int i = 0; i < Connected.length; i++){
+//                for(int j = 0; j < Connected.length; j++){
+//                    if(Connected[i][j] == 1){
+//                        ds.unionByRank(i, j);
+//                    }
+//                }
+//            }
+//            int count = 0;
+//            for(int i = 0; i < Connected.length; i++){
+//                if(ds.findParent(i) == i){
+//                    count++;
+//                }
+//            }
+//            return count;
+//        }
+//    }
+//    class DisjointSet {
+//        List<Integer> rank = new ArrayList<>();
+//        List<Integer> parent = new ArrayList<>();
+//        List<Integer> size = new ArrayList<>();
+//        DisjointSet(int n){
+//            for(int i = 0; i <= n; i++){
+//                rank.add(0);
+//                parent.add(i);
+//                size.add(1);
+//            }
+//        }
+//
+//        int findParent(int node){
+//            if(node == parent.get(node)){
+//                return node;
+//            }
+//            int ultimateParent = findParent(parent.get(node));
+//            parent.set(node, ultimateParent);
+//            return ultimateParent;
+//        }
+//
+//        void unionByRank(int u, int v){
+//            int ultimateParentU = findParent(u);
+//            int ultimateParentV = findParent(v);
+//            if(ultimateParentU == ultimateParentV){
+//                return;
+//            }
+//            if(rank.get(ultimateParentU) < rank.get(ultimateParentV)){
+//                parent.set(ultimateParentU, ultimateParentV);
+//            }
+//            else if(rank.get(ultimateParentV) < rank.get(ultimateParentU)){
+//                parent.set(ultimateParentV, ultimateParentU);
+//            }
+//            else{
+//                parent.set(ultimateParentV, ultimateParentU);
+//                rank.set(ultimateParentU, rank.get(ultimateParentU)+1);
+//            }
+//        }
+//    }
 }
